385ebf8e8fa802e5309977e31c3e639261542f98
Harsh Tomar <harshtomar6@gmail.com>
2020-03-18 21:14:07 +0530

    Finished get-started page

--- src/pages/get-started.js
+++ src/pages/get-started.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import Layout from './../components/layout'
-import SEO from './../components/seo'
-import Container from 'react-bootstrap/Container'
-import { Link } from 'gatsby';
-import { SERVER_URI } from './../globals'
+import React from "react"
+import Layout from "./../components/layout"
+import SEO from "./../components/seo"
+import Container from "react-bootstrap/Container"
+import { Link } from "gatsby"
+import { SERVER_URI } from "./../globals"
 
 export default function GetStarted() {
-
   return (
     <Layout>
       <SEO title="Get Started" />
@@ -16,38 +15,57 @@ export default function GetStarted() {
         <p className="lead">
           Integrate Captcha to your website in less than 5 minutes
         </p>
-        <br/>
+        <br />
 
         <h5>1. Signup and get API Keys</h5>
         <p className="ml-3">
-          You need to create an account to get an API key. 
+          You need to create an account to get an API key.
           <Link to="/signup"> Click here</Link> to create an account
         </p>
-        <br/>
+        <br />
 
         <h5>2. Add Captcha widget</h5>
         <p className="ml-3 mb-0">
           Paste the following code at the end of body to request for a captcha
         </p>
-        <code className="ml-3">
-          {`<script src="${SERVER_URI}/scripts/catpcha.js"></script>`}
-        </code>
-        <br/>
+        <pre className="p-2 bg-dark ml-3">
+          <code className="ml-3 text-light">
+            {`<script src="${SERVER_URI}/scripts/catpcha.js"></script>`}
+          </code>
+        </pre>
+        <p className="ml-3">
+          When this script is loaded, a variable <code>captchaId</code> is added to <code>window</code> object.
+          You need this variable later to validate the captcha.
+        </p>
         <p className="mt-2 ml-3 mb-0">
           To render Captcha, Add the following code in your HTML page
         </p>
-        <code className="ml-3">
-          {
-            `<div id="captcha-container" data-apikey="YOUR_API_KEY"></div>`
-          }
-        </code>
-        
-        <br/><br/>
+        <pre className="p-2 bg-dark ml-3">
+          <code className="text-light">
+            {`<div id="captcha-container" data-apikey="YOUR_API_KEY"></div>`}
+          </code>
+        </pre>
+        <small className="ml-3">* YOUR_API_KEY should be replaced by actual API Key </small>
+        <br />
+        <br />
 
         <h5>3. Validate Captcha</h5>
         <p className="ml-3">
-        Validate Captcha by sending a <code>POST</code> request to <code>{`${SERVER_URI}/captcha/validate`}</code>
+          Validate Captcha by sending a <code>POST</code> request to{" "}
+          <code>{`${SERVER_URI}/captcha/validate`}</code>. <br/>
+          Send following params as Request payload
         </p>
+        <pre className="p-2 ml-3 bg-dark">
+          <code className="text-light">
+            {`
+            {
+              "captchaId": window.captchaId,
+              "key": "YOUR_API_KEY",
+              "text": "TEXT_FROM_USER_INPUT"
+            }`
+            }
+          </code>
+        </pre>
       </Container>
     </Layout>
   )
